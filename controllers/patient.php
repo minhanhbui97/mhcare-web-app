72f54606492e731d83e824f5b5799b34963ba0eb
Minh Anh Bui <minhanhbui@MacBook-Air-Ellie.local>
2023-11-08 02:36:44 -0500

    Add columns doctor and referring doctor for patient view

--- controllers/patient.php
+++ controllers/patient.php
@@ -4,9 +4,26 @@
 $db_config = require('config.php');
 $db = new Database($db_config['database']);
 
+
+// Get patient info from patient table
 $id = $_GET['id'];
-$query = "SELECT * FROM patient WHERE patient_id = :id";
-$patient = $db->query($query,[':id' => $id])->find();
+$query_1 = "SELECT * FROM patient WHERE patient_id = :id";
+$patient = $db->query($query_1,[':id' => $id])->find();
+
+// Get doctor name from doctor table
+$query_2 = "SELECT CONCAT(employee.first_name, ' ', employee.last_name) as doctor_name FROM employee INNER JOIN doctor ON employee.employee_id = doctor.employee_id INNER JOIN patient ON doctor.employee_id = patient.doctor_id WHERE patient.patient_id = :id;";
+$doctor_name = $db->query($query_2,[':id' => $id])->find();
+
+// Get referring_doctor name from referring_doctor table
+$query_3 = "SELECT CONCAT(referring_doctor.first_name, ' ', referring_doctor.last_name) as referring_doctor_name FROM referring_doctor INNER JOIN patient ON patient.referring_doctor_id = patient.referring_doctor_id WHERE patient.patient_id = :id;";
+$referring_doctor_name = $db->query($query_3,[':id' => $id])->find();
+// dumpAndDie($referring_doctor_name); 
+
+
+// Get list of medications
+
+// Get list of allergies
+
 
 
 // dumpAndDie($patient); 
--- views/patient.view.php
+++ views/patient.view.php
@@ -1,4 +1,4 @@
-<?php $name = "Mao Phoc" ; ?>
+<?php $name = "Mao Phoc"; ?>
 
 <?php require('views/partials/head.php'); ?>
 
@@ -6,110 +6,126 @@
 
 
 <div class="container-fluid banner">
-    <div class="row align-items-center justify-content-between">
-        <div class="col-md image-container">
-        </div>
-
-        <div class="col-md">
-            <h1 class="text-light banner-text">
-                Welcome to MH-Care Employee Workspace
-            </h1>
-            <h2 class="text-light banner-text">
-                You are logged in as <?= $name ?>
-            </h2>
-        </div>
+  <div class="row align-items-center justify-content-between">
+    <div class="col-md image-container">
     </div>
+
+    <div class="col-md">
+      <h1 class="text-light banner-text">
+        Welcome to MH-Care Employee Workspace
+      </h1>
+      <h2 class="text-light banner-text">
+        You are logged in as <?= $name ?>
+      </h2>
+    </div>
+  </div>
 </div>
 
 <!-- Breadcrumb nav -->
 <div id="breadcrumb">
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item">Home</li>
-            <li class="breadcrumb-item"><a href="http://localhost/employee-workspace">Employee Workspace</a></li>
-            <li class="breadcrumb-item"><a href="http://localhost/patients">Patient List</a></li>
-            <li class="breadcrumb-item" aria-current="page">Patient Information</li>
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item">Home</li>
+      <li class="breadcrumb-item"><a href="http://localhost/employee-workspace">Employee Workspace</a></li>
+      <li class="breadcrumb-item"><a href="http://localhost/patients">Patient List</a></li>
+      <li class="breadcrumb-item" aria-current="page">Patient Information</li>
 
-        </ol>
-    </nav>
+    </ol>
+  </nav>
 </div>
 <div class="container">
-    <form class="form">
-      <div class="full-width text-center">
-        <h3>
-            Patient Information
-        </h3>
+  <form class="form">
+    <div class="full-width text-center">
+      <h3>
+        Patient Information
+      </h3>
 
-      </div>
+    </div>
 
 
-      <div>
-        <label class="form-label control-label full-width">Patient ID</label>
-        <input type="text" class="form-control" name="patient_id" value="<?= $patient['patient_id'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label full-width">Patient ID</label>
+      <input type="text" class="form-control" name="patient_id" value="<?= $patient['patient_id'] ?>" disabled readonly>
+    </div>
 
-      <br></br>
+    <br></br>
 
-      <div>
-        <label class="form-label control-label">First Name</label>
-        <input type="text" class="form-control" name="first_name" value="<?= $patient['first_name'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label">First Name</label>
+      <input type="text" class="form-control" name="first_name" value="<?= $patient['first_name'] ?>" disabled readonly>
+    </div>
 
-      <div>
-        <label class="form-label control-label">Last Name</label>
-        <input type="text" class="form-control" name="last_name" value="<?= $patient['last_name'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label">Last Name</label>
+      <input type="text" class="form-control" name="last_name" value="<?= $patient['last_name'] ?>" disabled readonly>
+    </div>
 
-      <div>
-        <label class="form-label control-label">Gender</label>
-        <input type="text" class="form-control" name="gender" value="<?= $patient['gender'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label">Gender</label>
+      <input type="text" class="form-control" name="gender" value="<?= $patient['gender'] ?>" disabled readonly>
+    </div>
 
-      <div>
-        <label class="form-label control-label">Date of Birth</label>
-        <input type="text" class="form-control" name="date_of_birth" value="<?= $patient['date_of_birth'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label">Date of Birth</label>
+      <input type="text" class="form-control" name="date_of_birth" value="<?= $patient['date_of_birth'] ?>" disabled readonly>
+    </div>
 
-      <div>
-        <label class="form-label control-label">Address 1</label>
-        <input type="text" class="form-control" name="address_1" value="<?= $patient['address_1'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label">Address 1</label>
+      <input type="text" class="form-control" name="address_1" value="<?= $patient['address_1'] ?>" disabled readonly>
+    </div>
 
-      <div>
-        <label class="form-label control-label">Address 2</label>
-        <input type="text" class="form-control" name="address_1" value="<?= $patient['address_2'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label">Address 2</label>
+      <input type="text" class="form-control" name="address_1" value="<?= $patient['address_2'] ?>" disabled readonly>
+    </div>
 
-      <div>
-        <label class="form-label control-label">City</label>
-        <input type="text" class="form-control" name="city" value="<?= $patient['city'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label">City</label>
+      <input type="text" class="form-control" name="city" value="<?= $patient['city'] ?>" disabled readonly>
+    </div>
 
-      <div>
-        <label class="form-label control-label">Province</label>
-        <input type="text" class="form-control" name="province" value="<?= $patient['province'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label">Province</label>
+      <input type="text" class="form-control" name="province" value="<?= $patient['province'] ?>" disabled readonly>
+    </div>
 
-      <div>
-        <label class="form-label control-label">Postal Code</label>
-        <input type="text" class="form-control" name="postal_code" value="<?= $patient['postal_code'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label">Postal Code</label>
+      <input type="text" class="form-control" name="postal_code" value="<?= $patient['postal_code'] ?>" disabled readonly>
+    </div>
 
-      <div>
-        <label class="form-label control-label">Phone Number</label>
-        <input type="tel" class="form-control" name="phone_number" value="<?= $patient['phone_number'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label">Phone Number</label>
+      <input type="tel" class="form-control" name="phone_number" value="<?= $patient['phone_number'] ?>" disabled readonly>
+    </div>
 
-      <div>
-        <label class="form-label control-label">Email</label>
-        <input type="email" class="form-control" name="email" value="<?= $patient['email'] ?>" disabled readonly>
-      </div>
+    <div>
+      <label class="form-label control-label">Email</label>
+      <input type="email" class="form-control" name="email" value="<?= $patient['email'] ?>" disabled readonly>
+    </div>
 
-    </form>
+    <div>
+      <label class="form-label control-label">Doctor</label>
+      <input type="email" class="form-control" name="doctor" value="<?= $doctor_name['doctor_name']?>" disabled readonly>
+    </div>
 
-    <a href="patients"><button class="btn btn-link"> Back </button></a>
+    <div>
+      <label class="form-label control-label">Referring Doctor</label>
+      <input type="email" class="form-control" name="referring_doctor" value="<?= $referring_doctor_name['referring_doctor_name']?>" disabled readonly>
+    </div>
     
-  </div>
+
+    <br>
+    <div>
+      <label><input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">I have a bike</label>
+    </div>
+
+  </form>
+
+  <a href="patients"><button class="btn btn-link"> Back </button></a>
+
+</div>
 
 <!-- <div class="index-text container-border">
     <h1 class="text-center"> Register New Patient </h1>
@@ -138,4 +154,3 @@
 </div> -->
 
 <?php require('views/partials/footer.php'); ?>
-
